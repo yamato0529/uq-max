187609a3c84baae88e798b30dfbf824f8e691140
yamato0529 <kohei.yamato0529@gmail.com>
2019-05-19 18:00:39 +0900

    project base create

---
+++ source/ios/uq-max/uq-max/AlermCell.swift
@@ -0,0 +1,16 @@
+//
+//  AlermCell.swift
+//  uq-max
+//
+//  Created by Yamato on 2019/05/19.
+//  Copyright © 2019 Kohei Yamato. All rights reserved.
+//
+import UIKit
+
+class AlermCell: UITableViewCell {
+    
+    @IBOutlet weak var timeLable: UILabel!
+    @IBOutlet weak var alermSwitch: UISwitch!
+    @IBOutlet weak var periodLabel: UILabel!
+
+}
