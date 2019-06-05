0046e54a546d0c22cfc4a66f4449fedfffdf5c91
yamato0529 <kohei.yamato0529@gmail.com>
2019-06-06 03:26:48 +0900

    時間登録処理を追加

--- source/ios/uq-max/uq-max/src/alerm/AlermAddViewController.swift
+++ source/ios/uq-max/uq-max/src/alerm/AlermAddViewController.swift
@@ -7,22 +7,35 @@
 //
 
 import UIKit
+import Pring
 
 class AlermAddViewController: UIViewController {
 
+    // DataSource
+    var dataSource: DataSource<Item>?
+    
     var receivedTextLabel: String?
     @IBOutlet weak var sleepTimePicker: UIDatePicker!
     @IBAction func backButton(_ sender: UIButton) {
         self.dismiss(animated: true, completion: nil)
 
     }
+
+    // 保存ボタン押下時
+    @IBAction func saveButton(_ sender: UIButton) {
+        
+        let item: Item = Item()
+        item.time = sleepTimePicker.date
+        item.save()
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         //UIDatePickerを.timeモードにする
         sleepTimePicker.datePickerMode = UIDatePicker.Mode.time
         //現在の時間をDatePickerに表示
         sleepTimePicker.setDate(Date(), animated: false)
-  
+        
         // Do any additional setup after loading the view.
     }
     
@@ -40,14 +53,13 @@ class AlermAddViewController: UIViewController {
             
             // NavigationControllerの一番目のViewControllerが次の画面
             let vc = nc.topViewController as! WeekTableViewController
-            
+
             // 次画面のテキスト表示用のStringに、本画面のテキストフィールドのテキストを入れる
 //            vc.receiveText = "auau"
             
         }
     }
 
-    
     /*
     // MARK: - Navigation
 
---
+++ source/ios/uq-max/uq-max/src/alerm/model/Alerm.swift
@@ -0,0 +1,16 @@
+//
+//  Alerm.swift
+//  uq-max
+//
+//  Created by Yamato on 2019/06/06.
+//  Copyright © 2019 Kohei Yamato. All rights reserved.
+//
+
+import Pring
+
+@objcMembers
+class Item: Object {
+    
+    dynamic var time: Date?
+    
+}
