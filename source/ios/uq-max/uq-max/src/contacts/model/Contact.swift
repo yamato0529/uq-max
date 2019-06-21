524bda9cccada2c916d86799d2190bff573e673e
yamato0529 <kohei.yamato0529@gmail.com>
2019-06-22 01:57:51 +0900

    連絡先追加機能追加

---
+++ source/ios/uq-max/uq-max/src/contacts/model/Contact.swift
@@ -0,0 +1,28 @@
+//
+//  Contact.swift
+//  uq-max
+//
+//  Created by Yamato on 2019/06/22.
+//  Copyright © 2019 Kohei Yamato. All rights reserved.
+//
+
+import Pring
+
+@objcMembers
+class Contact: Object {
+    
+    dynamic var companyName: String?
+    dynamic var companyKana: String?
+    dynamic var familyName: String?
+    dynamic var familyKana: String?
+    dynamic var firstName: String?
+    dynamic var firstKana: String?
+    dynamic var tel: String?
+    dynamic var mailAddress: String?
+    dynamic var mailTitle: String?
+    dynamic var mailBody: String?
+    dynamic var slackId: String?
+    dynamic var twitterId: String?
+    
+
+}
